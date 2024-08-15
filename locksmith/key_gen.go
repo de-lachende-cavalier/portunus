2274078a36d43f45b71e0ce34416bba59a0b7d53
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-08-15 22:24:41 +0200

    fixed and cleaned up the code

--- locksmith/key_gen.go
+++ locksmith/key_gen.go
@@ -1,26 +1,40 @@
 package locksmith
 
 import (
-	"errors"
+	"fmt"
+	"os"
 	"os/exec"
 )
 
-// Wrapper around ssh-keygen, generates private/public key pair with the given password.
+// Wrapper around ssh-keygen.
+// Generates private/public key pair with the given password.
 func genKeyPair(cipher string, passwd string, path string) error {
+	// removing the previous files is crucial, becuase ssh-keygen prompts an overwrite otherwise
+	os.Remove(path)
+	os.Remove(path + ".pub")
+
 	args := []string{"-q", "-t", cipher, "-N", passwd, "-f", path, "-a", "50"}
 
 	switch cipher {
 	case "rsa":
 		args = append(args, "-b", "4096")
-		return execCommand(args...)
 	case "ed25519":
-		return execCommand(args...)
 	default:
-		return errors.New("cipher not supported")
+		return fmt.Errorf("cipher not supported: %s", cipher)
 	}
-}
 
-// Helper function, to separate command execution from the definition of the various flags.
-func execCommand(args ...string) error {
-	return exec.Command("ssh-keygen", args...).Run()
+	cmd := exec.Command("ssh-keygen", args...)
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return err
+	}
+	if _, err := os.Stat(path + ".pub"); os.IsNotExist(err) {
+		return err
+	}
+
+	return nil
 }
